16b9c3c1d5b4b7e73c791f543a8eed67bc5cdfec
mustopha <mustophaqadir@gmail.com>
2023-03-03 09:49:02 -0800

    update

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,7 +4,7 @@
   * binary_tree_depth - Measures the depth of a node in a binary tree.
   * @tree: A pointer to the node to measure the depth.
   *
-  * Return: 
+  * Return:
   */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
